309c055346cdab045f47bc5f0fb84528d52f94d0
MariemAbdi <mariem.abdi2000@gmail.com>
2024-10-28 11:39:05 +0100

    web re-build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -40,8 +40,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "50c14edb9c939aaa9abb5c8fd2748806
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "91eb1401b96fe57c63a9d761ea2c3be7",
-"/": "91eb1401b96fe57c63a9d761ea2c3be7",
+"index.html": "6db065eaec200802245dd89acc0f04bf",
+"/": "6db065eaec200802245dd89acc0f04bf",
 "main.dart.js": "3fabf46db1a2c151117040b1bb4871d4",
 "manifest.json": "195aadef10d54c525bc4cf03c8182310",
 "version.json": "2cbed1e24a68cf43cf7a87c032558aaa"};
